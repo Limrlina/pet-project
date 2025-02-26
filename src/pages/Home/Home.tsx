a7dd6d8173c3e30d871c8041e7185d70cf35c9b6
Milana <milanaryz@mail.ru>
2025-02-26 14:06:39 +0300

    feat: add banners grid on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,31 @@
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
+import clsx from "clsx";
 
 const Home = () => {
-    return (
-        <div className={styles.container}>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles["hero-container"]}>
+        <p className={styles.hero}>
+          {"Мы — оптовый поставщик \n канцелярских" +
+            "и сопутствующих товаров \n для офиса и школы"}
+        </p>
+      </div>
+      <div className={styles["sales-container"]}>
+        <div className={clsx(styles["sale-banner"], styles["up-left"])}></div>
+        <div className={clsx(styles["sale-banner"], styles["down-left"])}></div>
+
+        <div className={clsx(styles["sale-banner"], styles["main"])}></div>
+
+        <div className={clsx(styles["sale-banner"], styles["up-right"])}></div>
+        <div
+          className={clsx(styles["sale-banner"], styles["down-right"])}
+        ></div>
+      </div>
+      <div className={styles["join-btn-container"]}>
+        <button>Стать нашим клиентом</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
